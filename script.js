55c825690cee5b9697767c513b3d4247f3b41047
smlfin <senioravp71@gmail.com>
2025-06-21 11:34:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- START: TWO-TIERED FRONT-END PASSWORD PROTECTION ---
-    const ACCESS_PASSWORD_FULL = "sml4576"; // Full access password
+    const ACCESS_PASSWORD_FULL = "biju123"; // Full access password
     const ACCESS_PASSWORD_LIMITED = "123";  // Limited access password
     let currentAccessLevel = null; // To store 'full' or 'limited'
     const accessDeniedOverlay = document.getElementById('accessDeniedOverlay');
